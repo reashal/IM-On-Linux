245ac8036b103a1e91556c92a34310e332193063
ruiyuqingshan <ruiyuqingshan@163.com>
2021-11-12 16:19:46 +0800

    客户端详细注释,准备封装

--- client.cpp
+++ client.cpp
@@ -16,29 +16,35 @@ int soc_cli,soc_rec,con_rec,fin,l_len;
 struct sockaddr_in  servaddr,cliaddr,localaddr;
 pthread_t rcv;
 
-void in_ip()
+void in_ip_defult()
 {
-  printf("--please input the server ip address:\n");
-  stpcpy(ip_address,"47.94.1.16");
-//  scanf("%s",ip_address);
-  // If you run the server program locally, illegal input other than the IP will still connect successfully
-  // but if run in the server,the illegal input will not successfully connect
-  // ( illegal input such as:"hhh" )
-//  std::cin.ignore();
-  // ignore '\n', or it will get error in connection  
+  char ip_defult[]="47.94.1.16";
+  stpcpy(ip_address,ip_defult);
+  // the defult ip in Aliyun  
+  return ;
+}
+
+void in_ip(char ip_input[])
+{
+  stpcpy(ip_address,ip_input);
+  /* If you run the server program locally, illegal input other than the IP will still connect local successfully.
+     but if run in the server,the illegal input will not successfully connect.
+     ( illegal input such as:"hhh" )*/
   return ;
 }
 
 int con_ser()
 {
   soc_cli=socket(AF_INET,SOCK_STREAM,0);
-  // set socket | error-1
+  // set socket | return socket description | error -1
   // AF_INET:ipv4  SOCK_STREAM:full duplex  0:automatic choose agreement of type(2)
   if(soc_cli==-1)
   {
     printf("--failed to create socket\n");
     return 0;
   }
+  // init
+  memset(&servaddr,0,sizeof(servaddr));
   servaddr.sin_family=AF_INET;
   // choose ipv4 but not ipv6
   servaddr.sin_port=htons(6666);
@@ -55,9 +61,7 @@ int con_ser()
     printf("--failed to connection\n");
     return 0;
   }
-  //printf("++%s\n",ip_address); ps:to tese local/server difference
-  // the client connect to the server
-  printf("--connection success!\n");
+  //printf("--connection success!\n");
   return 1;
 }
 
@@ -76,68 +80,58 @@ int send_info()
 
 void con_recv()
 {
-/*	printf("--the chile program\n");
-	cliaddr.sin_family=AF_INET;
-	cliaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-	cliaddr.sin_port=htons(6666);
-	listen(soc_cli,8);*/
-	printf("----I'm listening the server program\n");
+//	printf("----I'm listening the server program\n");
 	while(1)
 	{
 		bzero(msg_rec,sizeof(msg_rec));
-/*		if((con_rec=accept(soc_cli,(struct sockaddr*)NULL,NULL))==-1)
-			continue;
-		while(1)
-		{
-			fin=recv(con_rec,msg_rec,2222,0);
-			msg_rec[fin]='\0';
-			if(fin!=0) printf("--message from server: %s\n",msg_rec);
-			else break;
-		}
-	close(con_rec);*/
+		// The string is zeroed out each time a message is received
 		fin=read(soc_cli,msg_rec,sizeof(msg_rec));
 		if(!fin)
 		{
-			printf("----server is close\n");
+			printf("--The server program is close\n");
 			break;
 		}
-		printf("----");
+		printf("--Msg from server:");
 		fputs(msg_rec,stdout);
 		printf("\n");
 	}
 	close(soc_cli);
 	kill(getppid(),SIGUSR1);
 	exit(EXIT_SUCCESS);
-//	return;
+//	善后工作,关闭套接字/对接父进程
 }
 
-int main()
+int main()// 功能实现按数字标号,附注释
 {
-  // init
-  memset(&servaddr,0,sizeof(servaddr));
-  // input server ip address
-  in_ip();
-  // wait for result
+  /* 1.选择默认ip或者自定义ip,如果指向非本机的客户端,会检测ip输入是否合法*/
+  in_ip_defult();
+  // 默认ip,阿里云,后期调试/考核看情况去该函数里改
+  /*
+  scanf("%s",ip_address);
+  std::cin.ignore();
+  in_ip(ip_address);
+  // QT选择自定义ip,传参需要 **字符数组** 形式,用指针会报错
+  // 字符读入的时候需要ignore掉\n,否则会当成第一条消息发送
+  */
+  
+  /* 2.与服务器的连接部分,返回1则连接成功,返回0则图形界面自行决定处理逻辑,这里选择退出不进行重连
   printf("--please wait to see if the connection is succeed.\n");
+  printf("--you will get message in 2s if success\n");
+  */
   if(!con_ser()) return 0;
-  // connect failed
-  sleep(1); 
-  // connect buffer for success
-//  pthread_create(&rcv,NULL,con_recv,NULL);
-  // create pthread
-//  printf("--now you can send your message: \n");
-  // if(!send_info()) return 0;
+
+  //sleep(1); 非必须,仅Shell测试时为获取成功信息时美观设置
+  
+  /* 3.子进程接收消息*/
   pid_t k=fork();
   if(!k) con_recv();
-  while(1)
+  
+  /* 4.父进程发送消息*/
+  while(send_info())
   {
-    if(!send_info()) break;
-    // send failed. exit
+	  // 返回值为0时退出
   }
-//  pthread_join(rcv,NULL);
-  // del pthread
-//  con_recv();
   close(soc_cli);
-  // close socket. or the server program may have wrong
+  // 关闭套接字,否则服务端陷入接收消息的死循环
   return 0;
 }
