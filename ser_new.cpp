23ada0b7f8e42aaa9bf94d38eb6ba425e024bd89
ruiyuqingshan <ruiyuqingshan@163.com>
2021-11-13 03:34:30 +0800

    新版服务器端可以同时连接多个客户端.但仍有循环问题

---
+++ ser_new.cpp
@@ -0,0 +1,93 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<netdb.h>
+#include<signal.h>
+#include<unistd.h>
+#include<iostream>
+#include<pthread.h>
+#include<sys/types.h>
+#include<arpa/inet.h>    
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+int soc_ser,lst,acpt,fin,k,soc_lst;
+struct sockaddr_in seraddr,cliaddr;
+char msg_rec[2222],mess[2222];
+socklen_t l_size;
+
+int init()
+{
+	soc_ser=socket(AF_INET, SOCK_STREAM, 0);
+	if(soc_ser==-1)
+	{
+		printf("--failed creating socket description\n");
+		return 0;
+	}
+	l_size=sizeof(struct sockaddr_in);
+	memset(&seraddr,0,sizeof(seraddr));
+	seraddr.sin_family=AF_INET;
+	seraddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	seraddr.sin_port=htons(6666);
+//	soc_lst=socket(AF_INET,SOCK_STREAM,0);
+	if(bind(soc_ser,(struct sockaddr*)&seraddr,sizeof(seraddr))==-1)
+	{
+		printf("--failed bind\n");
+		return 0;
+	}
+	lst=listen(soc_ser,10);
+	if(lst==-1)
+	{
+		printf("--failed listen\n");
+		return 0;
+	}
+	printf("--Listen success,now begin accept:\n");
+	return 1;
+}
+
+int send_info()
+{
+  fgets(mess,2222,stdin);
+  // buffer | stop when '\n' what want to send 
+  if(send(soc_ser,mess,strlen(mess),0)==-1)
+  // (socket description,buffer,length,0)
+  {
+    printf("--failed to send this  message!\n");
+    return 0;
+  }
+  return 1;
+}
+
+void *ser_rcv(void *q)
+{
+	fin=0;
+	k=*(int *)q;
+	bzero(msg_rec,sizeof(msg_rec));
+	while(read(k,msg_rec,sizeof(msg_rec)))
+	{
+		// The string is zeroed out each time a message is received
+		printf("--Msg from client:");
+		fputs(msg_rec,stdout);
+		printf("\n");
+		bzero(msg_rec,sizeof(msg_rec));
+	}
+	close(k);
+}
+
+int main()
+{
+	if(!init())
+	  return 1;
+	while(1)
+	{
+		acpt=accept(soc_ser,(struct sockaddr *)&cliaddr,&l_size);
+		if(acpt!=-1)
+		{
+//			printf("------nn\n");
+			pthread_t k;
+			pthread_create(&k,NULL,ser_rcv,&acpt);
+		}
+	}
+	close(soc_ser);
+	return 0;
+}
